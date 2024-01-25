4783ba99a2452de6a2bd292b4084516ec7714912
Muhammad Rifqi Al Fauzan <rifqialfauzan03@gmail.com>
2024-01-25 10:18:40 +0700

    Done creating isAuthenticated function and hide profile or login-btn based on auth

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,34 @@
 function getTokenFromCookie(cookie) {
-    return undefined;
+    if (!cookie){
+        return null;
+    }
+
+    return cookie.split("; ")
+        .find((row) => row.startsWith("token="))
+        .split("=")[1];
 }
 
 function isTokenExpired(tokenFromCookie) {
-    // Use jwt decode to decode jwt to get the payload. And then grab the expiration from the payload
-    // and check if it expired or not
-    return false;
+    const base64Url = tokenFromCookie.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    const payload = JSON.parse(jsonPayload);
+    const tokenExp = payload.exp * 1000;
+
+    return Date.now() >= tokenExp;
 }
 
 export function isAuthenticated() {
     const tokenFromCookie = getTokenFromCookie(document.cookie);
+    console.info(tokenFromCookie);
     if (tokenFromCookie !== null) {
         if (!isTokenExpired(tokenFromCookie)){
             return true;
         }
     }
-
     return false;
 }
 
